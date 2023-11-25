ee5795b14463dd27095cffe9947b306cbd9356f8
najmul sakib <najmus191@gmail.com>
2023-11-25 12:47:49 +0600

    first commit

---
+++ src/main/kotlin/forLoop.kt
@@ -0,0 +1,17 @@
+//fun main(){
+//    var sum = 0
+//    for (i in 0..9){
+//        sum+=i
+//    }
+//    println(sum)
+//}
+
+
+fun main(){
+    for(i in 1..20)
+        if(i % 2 == 0){
+            println("Even number: $i")
+        }
+}
+
+
---
+++ src/main/kotlin/whileLoop.kt
@@ -0,0 +1,16 @@
+fun main() {
+    var sumResult = 0
+    var i = 0
+
+//    while (i < 10) {
+//        sumResult +=i
+//        i++
+//    }
+
+     do{
+        sumResult +=i
+        i++
+    }while (i < 10)
+
+    println("Sum: ${sumResult}")
+}
